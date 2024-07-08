103978715a342a97236d8f974e93df8940d3b46c
RockySheoran <rockysheoran72@gmail.com>
2024-07-08 22:45:36 +0530

    create 43 files

---
+++ e_commerce_project/main.js
@@ -0,0 +1,9 @@
+import './style.css'
+import products from "./api/products.json";
+// console.log(products);
+import { showProductContainer } from "./homeProductCards";
+
+// Define a function named `showProductContainer` that takes an array of products as input.
+showProductContainer(products);
+
+//e_commerce_project
